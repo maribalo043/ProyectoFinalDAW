143476295af214a016df14b2e287dbcef006e8a4
Mario <mariobl90@educastur.es>
2024-02-17 20:05:38 +0100

    CategoriaController empezado pero no terminado

--- Proyecto_MarioBajoLópez/src/main/java/com/mario/proyect/categoria/CategoriaController.java
+++ Proyecto_MarioBajoLópez/src/main/java/com/mario/proyect/categoria/CategoriaController.java
@@ -1,8 +1,26 @@
 package com.mario.proyect.categoria;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class CategoriaController {
+
+    @Autowired
+    CategoriaDAO categoriaDao;
+
+    @GetMapping("/categorias")
+    public ModelAndView getMethodName() {
+        
+        ModelAndView model = new ModelAndView();
+        model.setViewName("categorias");
+        model.addObject("categorias", categoriaDao.findAll());
+        
+        return model;
+    }
+    
     
 }
