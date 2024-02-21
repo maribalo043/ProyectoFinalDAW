bf6e3492623ee67f7880beba446193e3f8673203
Mario <mariobl90@educastur.es>
2024-02-21 19:08:24 +0100

    Partido Controller funcionando en principio

--- Proyecto_MarioBajoLópez/src/main/java/com/mario/proyect/partido/PartidoController.java
+++ Proyecto_MarioBajoLópez/src/main/java/com/mario/proyect/partido/PartidoController.java
@@ -1,13 +1,18 @@
 package com.mario.proyect.partido;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.mario.proyect.categoria.CategoriaDAO;
+import com.mario.proyect.equipo.Equipo;
 
 
 @Controller
@@ -56,11 +61,38 @@ public class PartidoController {
     	ModelAndView model = new ModelAndView();
     	model.setViewName("partidoForm");
     	model.addObject("partidoNuevo",new Partido());
-        model.addObject("categorias",categoriaDao.categoriasActive() );
+        model.addObject("categorias",categoriaDao.categoriasActive());
 
     	return model;  	
     }
 
+    @PostMapping("/partido/save")
+    public ModelAndView savePartido(@ModelAttribute Partido partidoNuevo) {
+
+        ModelAndView model = new ModelAndView();
+        model.setViewName("redirect:/partidos");
+        Optional<Partido> partidoOpcional =  partidoDao.findById(partidoNuevo.getId());
+        if (partidoOpcional.isPresent()) {
+            Partido existingPartido = partidoOpcional.get();
+            existingPartido.setId(partidoNuevo.getId());
+            existingPartido.setResultado(partidoNuevo.getResultado());
+            existingPartido.setPista(partidoNuevo.getPista());
+            existingPartido.setCategoria(partidoNuevo.getCategoria());
     
-    
+            partidoDao.save(existingPartido);
+        } else {
+            partidoDao.save(partidoNuevo);
+        }
+        return model;
+    }
+    @GetMapping("/partido/edit/{id}")
+    public ModelAndView editPartido(@PathVariable long id){
+
+        ModelAndView model = new ModelAndView();
+        model.setViewName("partidoForm");
+        model.addObject("partidoNuevo",partidoDao.findById(id));
+        model.addObject("categorias",categoriaDao.categoriasActive());
+
+        return model;
+    }
 }
