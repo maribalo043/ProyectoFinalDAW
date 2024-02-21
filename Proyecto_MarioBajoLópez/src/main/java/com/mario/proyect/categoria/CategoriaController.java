181d9260fc6bb7d7137b1791562bc8ab69fcac13
Mario <mariobl90@educastur.es>
2024-02-21 22:03:46 +0100

    Tablas Jugador, Categoria, Partido y Equipo terminado, dalta conectar la relacion N:M hacer el crud de la tabla intermedia, y buscar fallos de manera exaustiva, Puntos 1,2,4 TERMINADOS del proyecto

--- Proyecto_MarioBajoLópez/src/main/java/com/mario/proyect/categoria/CategoriaController.java
+++ Proyecto_MarioBajoLópez/src/main/java/com/mario/proyect/categoria/CategoriaController.java
@@ -1,11 +1,16 @@
 package com.mario.proyect.categoria;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.mario.proyect.equipo.Equipo;
 
 
 
@@ -43,7 +48,47 @@ public class CategoriaController {
 
         return model;
     }
-    
+    @GetMapping("/categoria/del/{id}")
+    public ModelAndView deleteCategoria(@PathVariable long id){
+
+        ModelAndView model = new ModelAndView();
+        model.setViewName("redirect:/categorias");
+        if(categoriaDao.findById(id).isPresent()){
+            categoriaDao.deleteById(id);
+        }
+        return model;
+    }
+    @PostMapping("categoria/save")
+    public ModelAndView saveCategoria(@ModelAttribute Categoria categoriaNueva){
+
+        ModelAndView model = new ModelAndView();
+        model.setViewName("redirect:/categorias");
+        Optional<Categoria> categoriaOpcional = categoriaDao.findById(categoriaNueva.getId());
+        if(categoriaOpcional.isPresent()){
+            Categoria categoria = categoriaOpcional.get();
+            categoria.setId(categoriaNueva.getId());
+            categoria.setNombre(categoriaNueva.getNombre());
+            categoria.setActiva(categoriaNueva.isActiva());
+            categoria.setEquipos(categoriaNueva.getEquipos());
+            categoria.setPartidos(categoriaNueva.getPartidos());
+
+            categoriaDao.save(categoria);
+        } else {
+            categoriaDao.save(categoriaNueva);
+        }
+        return model;
+    }
+    @GetMapping("categoria/edit/{id}")
+    public ModelAndView editCategoria(@PathVariable long id) {
+        
+        ModelAndView model = new ModelAndView();
+        Optional<Categoria> categoria = categoriaDao.findById(id);
+        if (categoria.isPresent()) {
+            model.addObject("categoriaNueva", categoria.get());
+        }
+        model.setViewName("categoriaForm");
+        return model;
+    }
     
     
     
--- Proyecto_MarioBajoLópez/src/main/java/com/mario/proyect/partido/PartidoController.java
+++ Proyecto_MarioBajoLópez/src/main/java/com/mario/proyect/partido/PartidoController.java
@@ -8,11 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.mario.proyect.categoria.CategoriaDAO;
-import com.mario.proyect.equipo.Equipo;
 
 
 @Controller
